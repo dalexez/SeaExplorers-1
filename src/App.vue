9c1e5fe6082a08478b9e47526aac76e71aca96c6
unciafidelis <unciafidelis@gmail.com>
2022-05-14 21:00:08 -0500

    Add: App vue and Landing

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,17 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-expand navbar-light bg-white">
-      <router-link to="/registryMember" class="navbar-brand"><DonationComponent/></router-link>
-      <div class="navbar-nav mr-auto">
-        <li class="nav-item">
-          <router-link to="/explorers" class="nav-link">Explorers</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link to="/add" class="nav-link">Agregar Explorer</router-link>
-        </li>
-         <li class="nav-item">
-          <router-link to="/addmcx" class="nav-link">Agregar Mission Commander</router-link>
-        </li>
-         <li class="nav-item">
-          <router-link to="/mcx" class="nav-link">Lista de Mission Commanders</router-link>
-        </li>
-      </div>
-    </nav>
-    <div class="container mt-3">
-      <router-view />
-    </div>
+    
+    <NavBar/>
+    <LandingPage/>
+    
   </div>
 </template>
 <script>
 import DonationComponent from "./components/DonationComponent.vue";
+import NavBar from "./components/NavBar.vue";
+import LandingPage from "./components/LandingPage.vue";
 export default {
     name: "app",
-    components: { DonationComponent }
+    components: { DonationComponent, NavBar, LandingPage }
 };
 </script>
